520ecdf27d33892cffc52710a783489929d5610e
kayzin.maung <kayzin.maung@KBZ-LPT-02146>
2024-10-08 22:44:08 +0630

    MVC Prj Restructure

---
+++ BookingSystem/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Reserve()
+        {
+            return View();
+        }
+
+
+        public IActionResult LoadMenu()
+        {
+            return PartialView("_Menu");
+        }
+
+        public IActionResult LoadAbout()
+        {
+            return PartialView("_About");
+        }
+
+        public IActionResult LoadReviews()
+        {
+            return PartialView("_Reviews");
+        }
+
+        public IActionResult Bookings()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ BookingSystem/Controllers/RestaurantController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSystem.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoadMenu()
+        {
+            return PartialView("_Menu");
+        }
+
+        public IActionResult LoadAbout()
+        {
+            return PartialView("_About");
+        }
+
+        public IActionResult LoadReviews()
+        {
+            return PartialView("_Reviews");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Bookings()
+        {
+            return View();
+        }
+    }
+}
